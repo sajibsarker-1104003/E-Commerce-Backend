466c58d4198f08828421c5f630f96ce07fa1b603
sajibsarker-1104003 <sajibpranto600@gmail.com>
2022-06-27 13:08:58 +0600

    Project Setup

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const morgan=require('morgan');
+
+app.use(express.json());
+app.use(cors());
+
+if(process.env.NODE_ENV==='development'){
+  app.use(morgan('dev'));
+}
+
+const app=express();
+
+module.exports = app;
