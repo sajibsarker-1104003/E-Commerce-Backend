a614a76c2594dd870d8e96b6ef37ed1ebdc969d4
sajibsarker-1104003 <sajibpranto600@gmail.com>
2022-07-04 16:08:26 +0600

    Node Product Cart=>Refactoring Codes

--- app.js
+++ app.js
@@ -2,23 +2,10 @@
 require('express-async-errors');
 const express = require('express');
 const app=express();
-const cors = require('cors');
-const morgan=require('morgan');
 
-const userRouter=require('./routers/userRouter');
-const categoryRouter=require('./routers/categoryRouter');
-const productRouter=require('./routers/productRouter');
 
-app.use(express.json());
-app.use(cors());
-
-if(process.env.NODE_ENV==='development'){
-  app.use(morgan('dev'));
-}
-
-app.use('/api/user',userRouter);
-app.use('/api/category',categoryRouter);
-app.use('/api/product',productRouter);
+require('./middlewares/index')(app);
+require('./middlewares/routes')(app);
 
 
 app.use((err,req,res,next)=>{
