7d8a2ec76f5f6747c2c7ae5d37068e8fdd1eab90
sajibsarker-1104003 <sajibpranto600@gmail.com>
2022-07-07 06:56:35 +0600

    Node – Payment Controller Part 2

--- controllers/paymentControllers.js
+++ controllers/paymentControllers.js
@@ -1,74 +1,74 @@
-const {CartItem}=require('../models/cartItem');
-const SSLCommerz = require("ssl-commerz-node");
-const PaymentSession = SSLCommerz.PaymentSession;
+const { CartItem } = require('../models/cartItem');
+const { Profile } = require('../models/profile');
+const PaymentSession = require('ssl-commerz-node').PaymentSession;
 
-module.exports.initPayment=async(req,res)=>{
+module.exports.initPayment = async (req, res) => {
+    const userId = req.user._id;
+    const cartItems = await CartItem.find({ user: userId });
+    const profile = await Profile.findOne({ user: userId });
 
-  const userId=req.user._id;
-  const cartItems=await CartItem.find({user:userId});
+    const { address1, address2, city, state, postcode, country, phone } = profile;
 
-  const total_amount=cartItems.map(item=>item.count*item.price).reduce((a,b)=>a+b,0);
-  const tran_id='_'+Math.random().toString(36).substr(2,9)+(new Date()).getTime();
+    const total_amount = cartItems.map(item => item.count * item.price)
+        .reduce((a, b) => a + b, 0);
 
+    const total_item = cartItems.map(item => item.count)
+        .reduce((a, b) => a + b, 0);
 
-  // For live payment set first parameter `false` and for sandbox set it `true`
-const payment = new PaymentSession(
-  true,
-  process.env.SSLCOMMERZ_STORE_ID,
-  process.env.SSLCOMMERZ_STORE_PASSWORD
-);
-// Set the urls
-payment.setUrls({
-  success: "yoursite.com/success", // If payment Succeed
-  fail: "yoursite.com/fail", // If payment failed
-  cancel: "yoursite.com/cancel", // If user cancel payment
-  ipn: "yoursite.com/ipn", // SSLCommerz will send http post request in this link
-});
+    const tran_id = '_' + Math.random().toString(36).substr(2, 9) + (new Date()).getTime();
 
-// Set order details
-payment.setOrderInfo({
-  total_amount: total_amount, // Number field
-  currency: "BDT", // Must be three character string
-  tran_id: tran_id, // Unique Transaction id
-  // emi_option: 0, // 1 or 0
-  // multi_card_name: "internetbank", // Do not Use! If you do not customize the gateway list,
-  // allowed_bin: "371598,371599,376947,376948,376949", // Do not Use! If you do not control on transaction
-  // emi_max_inst_option: 3, // Max instalment Option
-  // emi_allow_only: 0, // Value is 1/0, if value is 1 then only EMI transaction is possible
-});
+    const payment = new PaymentSession(true, process.env.STORE_ID, process.env.STORE_PASSWORD);
 
-// Set customer info
-payment.setCusInfo({
-  name: "Simanta Paul",
-  email: "simanta@bohubrihi.com",
-  add1: "66/A Midtown",
-  add2: "Andarkilla",
-  city: "Chittagong",
-  state: "Optional",
-  postcode: 4000,
-  country: "Bangladesh",
-  phone: "010000000000",
-  fax: "Customer_fax_id",
-});
+    // Set the urls
+    payment.setUrls({
+        success: 'yoursite.com/success', // If payment Succeed
+        fail: 'yoursite.com/fail', // If payment failed
+        cancel: 'yoursite.com/cancel', // If user cancel payment
+        ipn: 'yoursite.com/ipn' // SSLCommerz will send http post request in this link
+    });
 
-// Set shipping info
-payment.setShippingInfo({
-  method: "Courier", //Shipping method of the order. Example: YES or NO or Courier
-  num_item: 2,
-  name: "Simanta Paul",
-  add1: "66/A Midtown",
-  add2: "Andarkilla",
-  city: "Chittagong",
-  state: "Optional",
-  postcode: 4000,
-  country: "Bangladesh",
-});
+    // Set order details
+    payment.setOrderInfo({
+        total_amount: total_amount, // Number field
+        currency: 'BDT', // Must be three character string
+        tran_id: tran_id, // Unique Transaction id 
+        emi_option: 0, // 1 or 0
+    });
 
-// Set Product Profile
-payment.setProductInfo({
-  product_name: "Computer",
-  product_category: "Electronics",
-  product_profile: "general",
-});
-  
+    // Set customer info
+    payment.setCusInfo({
+        name: req.user.name,
+        email: req.user.email,
+        add1: address1,
+        add2: address2,
+        city: city,
+        state: state,
+        postcode: postcode,
+        country: country,
+        phone: phone,
+        fax: phone
+    });
+
+    // Set shipping info
+    payment.setShippingInfo({
+        method: 'Courier', //Shipping method of the order. Example: YES or NO or Courier
+        num_item: total_item,
+        name: req.user.name,
+        add1: address1,
+        add2: address2,
+        city: city,
+        state: state,
+        postcode: postcode,
+        country: country,
+    });
+
+    // Set Product Profile
+    payment.setProductInfo({
+        product_name: 'Sarker E-Commerce',
+        product_category: 'General',
+        product_profile: 'general'
+    });
+
+    response = await payment.paymentInit();
+    return res.status(200).send(response);
 }
