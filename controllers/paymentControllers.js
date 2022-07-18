899c7a679a010e5ac27851cabce5a13197a5d0a6
sajibsarker-1104003 <sajibpranto600@gmail.com>
2022-07-19 00:02:19 +0600

    Node – Accept Response in IPN URL

--- controllers/paymentControllers.js
+++ controllers/paymentControllers.js
@@ -28,7 +28,7 @@ module.exports.initPayment = async (req, res) => {
         success: 'yoursite.com/success', // If payment Succeed
         fail: 'yoursite.com/fail', // If payment failed
         cancel: 'yoursite.com/cancel', // If user cancel payment
-        ipn: 'yoursite.com/ipn' // SSLCommerz will send http post request in this link
+        ipn: 'https://lit-anchorage-13696.herokuapp.com/api/payment/ipn' // SSLCommerz will send http post request in this link
     });
 
     // Set order details
